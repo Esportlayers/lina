683d1cc96e68b3e5400524018c5bc8b2db5b3cb9
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-10-24 23:24:57 +0200

    Added callback url;

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ import LoginInfo from '../components/LoginInfo';
 
 export default function Home(): ReactElement {
   if(process.browser) {
-      if(localStorage.getItem('jwt')) {
-          Router.push('/grandCentralTerminal');
-      } else {
-          location.href = `${process.env.API_URL}/auth/twitch`;
-      }
+    if(localStorage.getItem('jwt')) {
+        Router.push('/grandCentralTerminal');
+    } else {
+        location.href = `${process.env.API_URL}/auth/twitch?callbackURL=${location.origin + '/auth'}`;
+    }
   }
 
   return <PageFrame>
