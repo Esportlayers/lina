753d0a86e26805d96f78fedc2300d9c4ac26345a
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-01-01 21:01:57 +0100

    Fixed 0 chatters issue;

--- components/Pages/Settings/Pages/VoteSystem/Pages/Rounds/ParticipantCellRenderer.tsx
+++ components/Pages/Settings/Pages/VoteSystem/Pages/Rounds/ParticipantCellRenderer.tsx
@@ -5,6 +5,6 @@ import { ReactElement } from "react";
 export default function ParticipantCellRenderer({chatters, total}: VoteRoundStats): ReactElement {
     const chatterParticipations = Math.floor((total * 100) / chatters);
     return <>
-        {total} ({chatterParticipations}%)
+        {total} {chatters > 0 && <>({chatterParticipations}%)</>}
     </>;
 }
