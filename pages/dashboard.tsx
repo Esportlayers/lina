bb6a181437dfca0b7493a128596cdb2a7af3cd6f
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-01-25 21:10:12 +0100

    fixed urls;

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 import PageFrame from '../components/PageFrame';
 import dynamic from "next/dynamic";
 import { useCurrentUser } from '../modules/selector/UiSelector';
-import {Wisp} from '@esportlayers/io';
+import {getWSUrl, Wisp} from '@esportlayers/io';
 import PageView from '../components/Pages/Dashboard/PageView';
 import VoteOverlay from '../components/VoteOverlay';
 import KeyWordListenerOverlay from '../components/KeyWordListenerOverlay/KeyWordListenerOverlay';
@@ -16,12 +16,12 @@ export default function Dashboard(): ReactElement {
     const currentUser = useCurrentUser();
     return <PageFrame>
         <div className={'page'}>
-            {currentUser && <Tether url={'wss://api.streamdota.com/dota-gsi/live/81f8c9aa-ed7b-47be-8f17-9ad01cb47fff'}>
+            {currentUser && <Tether url={getWSUrl(process.env.API_URL + '/dota-gsi/live/' + currentUser.frameApiKey)}>
                 <PageView />
                 <KeyWordListenerOverlay />
             </Tether>}
 
-            {currentUser && <Wisp url={'wss://api.streamdota.com/bets/live/81f8c9aa-ed7b-47be-8f17-9ad01cb47fff'}>
+            {currentUser && <Wisp url={getWSUrl(process.env.API_URL + '/bets/live/' + currentUser.frameApiKey)}>
                 <VoteOverlay />
             </Wisp>}
         </div>
