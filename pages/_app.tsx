cf5f140107d31f73130348766d2f0dad0307a278
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-02-14 15:42:11 +0100

    Added sentry io reporting;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import { FC } from "react";
 import { wrapper } from '../modules/Store';
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
+import * as SentryBrowser from '@sentry/browser';
 
 if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
     LogRocket.init('07xsnw/streamdota');
     setupLogRocketReact(LogRocket);
+    if(process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
+        SentryBrowser.init({dsn: process.env.SENTRY_DSN});
+    }
 }
 
 const WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
