7545eacc82beabaa433c5761ee6bae61c9d36809
Michael Ketzer <michael.ketzer@hcom.de>
2021-02-15 09:02:28 +0100

    Switched to @sentry/react;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import { FC } from "react";
 import { wrapper } from '../modules/Store';
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
-import * as SentryBrowser from '@sentry/browser';
+import * as Sentry from "@sentry/react";
 
 if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
     LogRocket.init('07xsnw/streamdota');
     setupLogRocketReact(LogRocket);
     if(process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
-        SentryBrowser.init({dsn: process.env.SENTRY_DSN});
+        Sentry.init({dsn: process.env.SENTRY_DSN});
     }
 }
 
