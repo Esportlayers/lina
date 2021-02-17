7bb5a91cfe7239f399e63eac0e798bab189fc875
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-02-17 20:25:37 +0100

    Removed log rocket;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import { AppProps } from 'next/app';
 import { FC } from "react";
 import { wrapper } from '../modules/Store';
-import LogRocket from 'logrocket';
-import setupLogRocketReact from 'logrocket-react';
 import * as Sentry from "@sentry/react";
 
-if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
-    LogRocket.init('07xsnw/streamdota');
-    setupLogRocketReact(LogRocket);
-    if(process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
-        Sentry.init({dsn: process.env.SENTRY_DSN});
-    }
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production' && process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
+    Sentry.init({dsn: process.env.SENTRY_DSN});
 }
 
 const WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
