2fa618973f98a8233928eda3b1df7a9295b9b09f
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-03-02 11:15:01 +0100

    Added user identifier;

--- modules/selector/UiSelector.ts
+++ modules/selector/UiSelector.ts
@@ -1,33 +1,55 @@
-import { User } from "@streamdota/shared-types";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { loadCurrentUser } from "../reducer/Ui";
-import { State } from "../Store";
+import * as Sentry from "@sentry/react";
 
-export const currentUserSelector = (state: State): User | null => state.ui.currentUser;
-export const loadedVoteSeasonsSelector = (state: State): boolean => state.ui.loadedEntities.voteSeasons; 
-export const loadedVoteOverlaySelector = (state: State): boolean => state.ui.loadedEntities.voteOverlay; 
-export const loadedDotaOverlaySelector = (state: State): boolean => state.ui.loadedEntities.dotaOverlay; 
-export const loadedVoteRoundsSelector = (state: State): number[] => state.ui.loadedEntities.voteRounds;
-export const loadedVoteSeasonToplistSelector = (state: State): number[] => state.ui.loadedEntities.voteSeasonToplist;
-export const loadedVoteSeasonStatsSelector = (state: State): number[] => state.ui.loadedEntities.voteSeasonStats;
-export const loadedBotCommandsSelector = (state: State): boolean => state.ui.loadedEntities.botCommands;
-export const loadedGoogleFontsSelector = (state: State): boolean => state.ui.loadedEntities.googleFonts;
-export const loadedWordGroupsSelector = (state: State): boolean => state.ui.loadedEntities.wordGroups; 
-export const loadedWordGroupAnalysesSelector = (state: State): number[] => state.ui.loadedEntities.wordGroupAnalyses; 
+import { useDispatch, useSelector } from "react-redux";
+
+import { State } from "../Store";
+import { User } from "@streamdota/shared-types";
+import { loadCurrentUser } from "../reducer/Ui";
+import { useEffect } from "react";
+
+export const currentUserSelector = (state: State): User | null =>
+  state.ui.currentUser;
+export const loadedVoteSeasonsSelector = (state: State): boolean =>
+  state.ui.loadedEntities.voteSeasons;
+export const loadedVoteOverlaySelector = (state: State): boolean =>
+  state.ui.loadedEntities.voteOverlay;
+export const loadedDotaOverlaySelector = (state: State): boolean =>
+  state.ui.loadedEntities.dotaOverlay;
+export const loadedVoteRoundsSelector = (state: State): number[] =>
+  state.ui.loadedEntities.voteRounds;
+export const loadedVoteSeasonToplistSelector = (state: State): number[] =>
+  state.ui.loadedEntities.voteSeasonToplist;
+export const loadedVoteSeasonStatsSelector = (state: State): number[] =>
+  state.ui.loadedEntities.voteSeasonStats;
+export const loadedBotCommandsSelector = (state: State): boolean =>
+  state.ui.loadedEntities.botCommands;
+export const loadedGoogleFontsSelector = (state: State): boolean =>
+  state.ui.loadedEntities.googleFonts;
+export const loadedWordGroupsSelector = (state: State): boolean =>
+  state.ui.loadedEntities.wordGroups;
+export const loadedWordGroupAnalysesSelector = (state: State): number[] =>
+  state.ui.loadedEntities.wordGroupAnalyses;
 
 export function useCurrentUser(auth?: string): User | null {
-	const user = useSelector(currentUserSelector);
-	const dispatch = useDispatch();
+  const user = useSelector(currentUserSelector);
+  const dispatch = useDispatch();
 
-	useEffect(
-        () => {
-			if (!user) {
-				dispatch(loadCurrentUser(auth));
-			}
-		},
-		[ auth, user ]
-	);
+  useEffect(() => {
+    if (!user) {
+      dispatch(loadCurrentUser(auth));
+    } else if (
+      typeof window !== "undefined" &&
+      process.env.NODE_ENV === "production" &&
+      process.env.SENTRY_DSN &&
+      process.env.SENTRY_DSN.length > 0
+    ) {
+      Sentry.setUser({
+        id: `${user.id}`,
+        username: user.displayName,
+        twitchId: user.twitchId,
+      });
+    }
+  }, [auth, user]);
 
-	return user;
+  return user;
 }
