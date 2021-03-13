da24b126863f8d41719e0808a734e5f2ded0075a
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-03-13 18:24:31 +0100

    Added abortcontroller polyfill;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,28 @@
-import { AppProps } from 'next/app';
-import { FC } from "react";
-import { wrapper } from '../modules/Store';
+import "abortcontroller-polyfill/dist/abortcontroller-polyfill-only";
+
 import * as Sentry from "@sentry/react";
 
-if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production' && process.env.SENTRY_DSN && process.env.SENTRY_DSN.length > 0) {
-    Sentry.init({dsn: process.env.SENTRY_DSN});
+import { AppProps } from "next/app";
+import { FC } from "react";
+import { wrapper } from "../modules/Store";
+
+if (
+  typeof window !== "undefined" &&
+  process.env.NODE_ENV === "production" &&
+  process.env.SENTRY_DSN &&
+  process.env.SENTRY_DSN.length > 0
+) {
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
 }
 
-const WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
-    <Sentry.ErrorBoundary fallback={"An error has occurred. This error was reported. Please try reloading the page!"}>
-        <Component {...pageProps} />
-    </Sentry.ErrorBoundary>
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
+  <Sentry.ErrorBoundary
+    fallback={
+      "An error has occurred. This error was reported. Please try reloading the page!"
+    }
+  >
+    <Component {...pageProps} />
+  </Sentry.ErrorBoundary>
 );
 
 export default wrapper.withRedux(WrappedApp);
